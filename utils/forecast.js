c4ca1e5d2d3b045f98fa21db394bb1f2e7c34648
vectorNull <jose.rodriguez@prominentcode.com>
2021-08-18 12:45:46 -0700

    Initial commit

---
+++ utils/forecast.js
@@ -0,0 +1,25 @@
+const request = require('postman-request')
+
+const forecast = (longitude, latitude, callback) => {
+    const url = `http://api.weatherstack.com/current?access_key=bd6f3134aca9eac29a8c9ffe12fda587&units=f&query=${latitude},${longitude}`;
+    
+    request({url: url, json: true}, (error, response, body) => {
+        if(error) {
+            callback('Unable to connect to weather service.');
+        } else if (body.error) {
+            const { code, type, info } = body.error;
+            callback({
+                statusCode: code,
+                type,
+                message: info
+            });
+        } else {
+            const temp = body.current.temperature;
+            const feelsLike = body.current.feelslike;
+            callback(undefined, `The current temperature is ${temp} degrees, but it feels like ${feelsLike} degrees.`);
+        }
+    
+    })
+}
+
+module.exports = forecast
